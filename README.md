e05a26090f75805bf505fd0d79c6665ebe172bda
Leonie <leonie.alexandra22@gmail.com>
2023-01-18 18:23:16 -0500

    Create files and folders

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cot-4500-intro
+Mention requirements.txt
+  Figure out how to incorporate and use requirements.txt (use this to install third party libraries)
+
+Mention of running python
+  Must include instructions on how to run intro_to_python.py from the command line
---
+++ src/main/__init__.py
@@ -0,0 +1,15 @@
+# i refer to row index and j refers to column index
+import numpy as np
+#expected output
+# 3 prints 
+
+def first_print():
+  for i in range(3):
+    for j in range(3):
+
+        
+  return " \n"  + " "
+
+if __name__ == "__main__":
+
+  print(first_print())
