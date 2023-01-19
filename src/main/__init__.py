23b5f36fd7a5fad85c078fdb04b8d647aa737f93
Leonie <leonie.alexandra22@gmail.com>
2023-01-18 20:28:49 -0500

    Finalised code

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -3,13 +3,33 @@ import numpy as np
 #expected output
 # 3 prints 
 
-def first_print():
-  for i in range(3):
-    for j in range(3):
-
-        
-  return " \n"  + " "
-
 if __name__ == "__main__":
+  #1. Print a specific 3x3 matrix where a cell is 1 if i == j, else 0 
+  #self note: can also use np.eye(3)
+  firstMatrix = np.array([[0,0,0],[0,0,0],[0,0,0]])
+  for i in 0, 1, 2:
+    for j in 0, 1, 2:
+      if(i == j):
+        firstMatrix[i][j] += 1
+  
+  for row in firstMatrix:
+    print(" ".join(map(str,row)))
 
-  print(first_print())
+  #2. Print the 3x3 matrix from #1 and then add 3 to every cell where i ≠j  
+  secondMatrix = np.array([[0, 0, 0],[0,0,0],[0,0,0]])
+  for i in 0, 1, 2:
+    for j in 0, 1, 2:
+      if(i != j):
+        secondMatrix[i][j] += 3
+  
+  print("\n")
+
+  for row in secondMatrix:
+    print(" ".join(map(str,row)))
+  
+  print("\n")
+
+  #3. Print the 3x3 matrix from #2 as a 3x2 by deleting the last column from the matrix created 
+  thirdMatrix = np.delete(secondMatrix, 2, 1)
+  for row in thirdMatrix:
+    print(" ".join(map(str,row)))
